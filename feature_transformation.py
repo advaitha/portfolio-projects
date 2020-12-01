1a583f893503e2902c20973047cd7ca6160b1222
Advaitha <tulasiram.gunipati@gmail.com>
2020-12-01 07:54:01 +0530

    updated XGboost py

--- feature_transformation.py
+++ feature_transformation.py
@@ -162,7 +162,8 @@ print(pca.explained_variance_ratio_)
 # Train Test split
 # use stratify in case of imbalance in class labels
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, stratify = y)
+X_train, X_test, y_train, y_test = train_test_split(X,y, stratify = y,
+                                                    test_size = 0.2, random_state = 123)
 
 
 #KNN is in linear space?
